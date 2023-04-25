814e5a4dec86bcec710ba277d52904f78dbbf881
oserifogullari18 <72516713+oserifogullari18@users.noreply.github.com>
2023-04-25 22:19:13 +0300

    points added

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 typedef vec4  color4;
 typedef vec4  point4;
 
-const int NumVertices = 36; //(6 faces)(2 triangles/face)(3 vertices/triangle)
+const int NumVertices = 324; //(6 faces)(2 triangles/face)(3 vertices/triangle)
 
 point4 points[NumVertices];
 color4 colors[NumVertices];
@@ -15,15 +15,41 @@ color4 colors[NumVertices];
 
 
 // Vertices of a unit cube centered at origin, sides aligned with axes
-point4 vertices[8] = {
-    point4( -0.5, -0.5,  0.5, 1.0 ),
-    point4( -0.5,  0.5,  0.5, 1.0 ),
-    point4(  0.5,  0.5,  0.5, 1.0 ),
-    point4(  0.5, -0.5,  0.5, 1.0 ),
-    point4( -0.5, -0.5, -0.5, 1.0 ),
-    point4( -0.5,  0.5, -0.5, 1.0 ),
-    point4(  0.5,  0.5, -0.5, 1.0 ),
-    point4(  0.5, -0.5, -0.5, 1.0 )
+point4 vertices[] = {
+    point4( -0.6, -0.6, -0.3, 1.0 ),
+    point4( -0.6, -0.3, -0.3, 1.0 ),
+    point4( -0.3, -0.3, -0.3, 1.0 ),
+    point4( -0.3, -0.6, -0.3, 1.0 ),
+    point4( -0.6, -0.6, -0.6, 1.0 ),
+    point4( -0.6, -0.3, -0.6, 1.0 ),
+    point4( -0.3, -0.3, -0.6, 1.0 ),
+    point4( -0.3, -0.6, -0.6, 1.0 ),
+    point4(  0.0, -0.3, -0.6, 1.0 ),
+    point4(  0.0, -0.6, -0.6, 1.0 ),
+    point4(  0.0, -0.3, -0.3, 1.0 ),
+    point4(  0.0, -0.6, -0.3, 1.0 ),
+    point4(  0.3, -0.3, -0.6, 1.0 ),
+    point4(  0.3, -0.6, -0.6, 1.0 ),
+    point4(  0.3, -0.3, -0.3, 1.0 ),
+    point4(  0.3, -0.6, -0.3, 1.0 ),
+    
+    point4( -0.6, 0.0, -0.3, 1.0 ),
+    point4( -0.6, 0.0, -0.6, 1.0 ),
+    point4( -0.3, 0.0, -0.6, 1.0 ),
+    point4( -0.3, 0.0, -0.3, 1.0 ),
+    point4(  0.0, 0.0, -0.3, 1.0 ),
+    point4(  0.0, 0.0, -0.6, 1.0 ),
+    point4(  0.3, 0.0, -0.6, 1.0 ),
+    point4(  0.3, 0.0, -0.3, 1.0 ),
+    
+    point4( -0.6, 0.3, -0.3, 1.0 ),
+    point4( -0.6, 0.3, -0.6, 1.0 ),
+    point4( -0.3, 0.3, -0.6, 1.0 ),
+    point4( -0.3, 0.3, -0.3, 1.0 ),
+    point4(  0.0, 0.3, -0.3, 1.0 ),
+    point4(  0.0, 0.3, -0.6, 1.0 ),
+    point4(  0.3, 0.3, -0.6, 1.0 ),
+    point4(  0.3, 0.3, -0.3, 1.0 ),
 };
 
 
@@ -43,7 +69,7 @@ color4 vertex_colors[8] = {
 // Array of rotation angles (in degrees) for each coordinate axis
 enum { Xaxis = 0, Yaxis = 1, Zaxis = 2, NumAxes = 3 };
 int      Axis = Xaxis;
-GLfloat  Theta[NumAxes] = { 5, 5, 0 };
+GLfloat  Theta[NumAxes] = { 5, 10, 0 };
 
 // Model-view and projection matrices uniform location
 GLuint  ModelView, Projection;
@@ -53,14 +79,15 @@ GLuint  ModelView, Projection;
 // quad generates two triangles for each face and assigns colors to the vertices
 int Index = 0;
 
-void quad( int a, int b, int c, int d )
+void quad( int a, int b, int c, int d, int color )
 {
-    colors[Index] = vertex_colors[a]; points[Index] = vertices[a]; Index++;
-    colors[Index] = vertex_colors[a]; points[Index] = vertices[b]; Index++;
-    colors[Index] = vertex_colors[a]; points[Index] = vertices[c]; Index++;
-    colors[Index] = vertex_colors[a]; points[Index] = vertices[a]; Index++;
-    colors[Index] = vertex_colors[a]; points[Index] = vertices[c]; Index++;
-    colors[Index] = vertex_colors[a]; points[Index] = vertices[d]; Index++;
+    colors[Index] = vertex_colors[color]; points[Index] = vertices[a]; Index++;
+    colors[Index] = vertex_colors[color]; points[Index] = vertices[b]; Index++;
+    colors[Index] = vertex_colors[color]; points[Index] = vertices[c]; Index++;
+    colors[Index] = vertex_colors[color]; points[Index] = vertices[a]; Index++;
+    colors[Index] = vertex_colors[color]; points[Index] = vertices[c]; Index++;
+    colors[Index] = vertex_colors[color]; points[Index] = vertices[d]; Index++;
+
 }
 
 //----------------------------------------------------------------------------
@@ -69,12 +96,62 @@ void quad( int a, int b, int c, int d )
 
 void colorcube()
 {
-    quad( 1, 0, 3, 2 );
-    quad( 2, 3, 7, 6 );
-    quad( 3, 0, 4, 7 );
-    quad( 6, 5, 1, 2 );
-    quad( 4, 5, 6, 7 );
-    quad( 5, 4, 0, 1 );
+    quad( 1, 0, 3, 2, 1 );
+    quad( 2, 3, 7, 6, 2 );
+    quad( 3, 0, 4, 7, 3 );
+    quad( 6, 5, 1, 2, 6 );
+    quad( 4, 5, 6, 7, 4 );
+    quad( 5, 4, 0, 1, 5 );
+    quad( 2, 3, 11, 10, 1 );
+    quad( 10, 11, 9, 8, 2 );
+    quad( 11, 3, 7, 9, 3 );
+    quad( 8, 6, 2, 10, 6 );
+    quad( 7, 6, 8, 9, 4 );
+    quad( 6, 7, 3, 2, 5 );
+    quad( 10, 11, 15, 14, 1 );
+    quad( 14, 15, 13, 12, 2 );
+    quad( 15, 11, 9, 13, 3 );
+    quad( 12, 8, 10, 14, 6 );
+    quad( 9, 8, 12, 13, 4 );
+    quad( 8, 9, 11, 10, 5 );
+
+    quad( 16, 1, 2, 19, 1 );
+    quad( 19, 2, 6, 18, 2 );
+    quad( 4, 1, 5, 6, 3 );
+    quad( 18, 17, 16, 19, 6 );
+    quad( 5, 17, 18, 6, 4 );
+    quad( 17, 5, 1, 16, 5 );
+    quad( 19, 2, 10, 20, 1 );
+    quad( 20, 10, 8, 21, 2 );
+    quad( 10, 2, 6, 8, 3 );
+    quad( 21, 18, 19, 20, 6 );
+    quad( 6, 18, 21, 8, 4 );
+    quad( 18, 6, 2, 19, 5 );
+    quad( 20, 10, 14, 23, 1 );
+    quad( 23, 14, 12, 22, 2 );
+    quad( 14, 10, 8, 12, 3 );
+    quad( 22, 21, 20, 23, 6 );
+    quad( 8, 21, 22, 12, 4 );
+    quad( 21, 8, 10, 20, 5 );
+    
+    quad( 24, 16, 19, 27, 1 );
+    quad( 27, 19, 18, 26, 2 );
+    quad( 5, 16, 17, 18, 3 );
+    quad( 26, 25, 24, 27, 6 );
+    quad( 17, 25, 26, 18, 4 );
+    quad( 25, 17, 16, 24, 5 );
+    quad( 27, 19, 20, 28, 1 );
+    quad( 28, 20, 21, 29, 2 );
+    quad( 20, 19, 18, 21, 3 );
+    quad( 29, 26, 27, 28, 6 );
+    quad( 18, 26, 29, 21, 4 );
+    quad( 26, 18, 19, 27, 5 );
+    quad( 28, 20, 23, 31, 1 );
+    quad( 31, 23, 22, 30, 2 );
+    quad( 23, 20, 21, 22, 3 );
+    quad( 30, 29, 28, 31, 6 );
+    quad( 21, 29, 30, 22, 4 );
+    quad( 29, 21, 20, 28, 5 );
 }
 
 
@@ -96,7 +173,6 @@ void init()
     GLuint vao;
     glGenVertexArrays( 1, &vao );
     glBindVertexArray( vao );
-    
     
     // Create and initialize a buffer object
     GLuint buffer;
@@ -123,9 +199,10 @@ void init()
     ModelView = glGetUniformLocation( program, "ModelView" );
     Projection = glGetUniformLocation( program, "Projection" );
     
+    
     // Set projection matrix
     mat4  projection;
-    projection = Ortho(-5.0, 5.0, -5.0, 5.0, -1.0, 1.0); // Ortho(): user-defined function in mat.h
+    projection = Ortho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0); // Ortho(): user-defined function in mat.h
     glUniformMatrix4fv( Projection, 1, GL_TRUE, projection );
     
     glEnable( GL_DEPTH_TEST );
@@ -140,22 +217,19 @@ void init()
 void display(void)
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    
-    for(int i = 0; i<3; i++){
-        for(int j=0; j<3; j++){
-                const vec3 displacement( 0.0 + i, 0.0 - j, 0.0);
-                mat4  model_view = (  Translate( displacement ) * Scale(1.0, 1.0, 1.0)
-                                    *
-                                    RotateX( Theta[Xaxis] * (3-j) ) *
-                                    RotateY( Theta[Yaxis] * (3-i)) *
-                                    RotateZ( Theta[Zaxis] )
-                                    );  // Scale(), Translate(), RotateX(), RotateY(), RotateZ(): user-defined functions in mat.h
-                
-                glUniformMatrix4fv( ModelView, 1, GL_TRUE, model_view );
-                glDrawArrays( GL_TRIANGLES, 0, NumVertices );
-                glFlush();
-        }
-    }
+    const vec3 displacement( 0.0 , 0.0 , 0.0);
+    mat4  model_view = (  Translate( displacement ) * Scale(1.0, 1.0, 1.0)
+                            *
+                            RotateX( Theta[Xaxis] ) *
+                            RotateY( Theta[Yaxis] ) *
+                            RotateZ( Theta[Zaxis] )
+                            );  // Scale(), Translate(), RotateX(), RotateY(), RotateZ(): user-defined functions in mat.h
+    glUniformMatrix4fv( ModelView, 1, GL_TRUE, model_view );
+    glDrawArrays( GL_TRIANGLES, 0, NumVertices );
+    glFlush();
+
+            
+
 }
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
@@ -186,6 +260,25 @@ void update( void )
         Theta[Axis] -= 360.0;
     }
 }
+
+void translateVertices(GLfloat* vertices, GLfloat* newVertices, float xAmount, float yAmount, float zAmount, int vertexAttributeNumber, int iterationNumber)
+{
+    for (int i = 0; i < iterationNumber; i++) {
+        newVertices[i] = vertices[i];
+
+        if(xAmount != 0 && i%vertexAttributeNumber % 9 == 0) {
+            newVertices[i] += xAmount;
+        }
+
+        if(yAmount != 0 && i%vertexAttributeNumber % 9 == 1) {
+            newVertices[i] += yAmount;
+        }
+
+        if(zAmount != 0 && i%vertexAttributeNumber % 9 == 2) {
+            newVertices[i] += zAmount;
+        }
+    }
+};
 
 //---------------------------------------------------------------------
 //
@@ -235,6 +328,9 @@ int main()
     glfwTerminate();
     exit(EXIT_SUCCESS);
 }
+
+
+
 
 
 
